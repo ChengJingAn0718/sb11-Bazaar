a61ab6cfc2390ca3f09428d75cf303aa61374fb0
BlueDream0718 <104906327+Cheng0718@users.noreply.github.com>
2022-05-27 01:30:32 -0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -65,7 +65,8 @@ const App = React.forwardRef(({ geo, _startTransition, baseGeo, bgLoaded }, ref)
   }
 
   function loadFunc() {
-    sceneList[currentSceneNum + 1].current.sceneLoad()
+    if (currentSceneNum > 0)
+      sceneList[currentSceneNum + 1].current.sceneLoad()
   }
 
   function loadScene() {
@@ -79,12 +80,12 @@ const App = React.forwardRef(({ geo, _startTransition, baseGeo, bgLoaded }, ref)
 
     audioList.backAudio.pause()
     audioList.backAudio.currentTime = 0;
-    
+
 
     setTimeout(() => {
       sceneList[1].current.sceneLoad()
       sceneList[0].current.sceneLoad()
-    }, 100);
+    }, 500);
 
     musicRef.current.setClass('hideObject')
 
